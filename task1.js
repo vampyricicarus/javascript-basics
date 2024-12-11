d7ad711dcf4b44df8217ade8981c9c3274f1e247
Darbi <darbiicarus@gmail.com>
2024-12-11 13:01:35 -0500

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,30 @@
+studentName = "Susan";
+studentAge = 16;
+studentGrade = 100;
+isPresent = true;
+
+console.log(studentName);
+console.log(studentAge);
+console.log(studentGrade);
+console.log(isPresent);
+
+x = 1;
+y = 4;
+
+console.log("Sum: ", x + y);
+console.log("Difference: ", x - y);
+console.log("Product: ", x * y);
+console.log("Quotient: ", x / y);
+
+console.log("Updated x: ", x + 4);
+console.log("Updated y: ", y - 3);
+
+console.log("Is x equal to y?", x === y);
+console.log("Is x greater than y?", x > y);
+console.log("Is x not equal to y?", x != y);
+
+let isPositive = num1;
+let isEven = num2;
+
+console.log("Are both numbers positive?", isPositive);
+console.log("Is at least one number even?", isEven);
